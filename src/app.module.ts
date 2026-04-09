36716eb9251e935fe3178fe759b98044591cc491
lucamoro <lu.moro89@gmail.com>
2026-04-09 06:39:53 +0200

    added users dto and post method for signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -6,9 +6,6 @@ export class User {
   id: number;
 
   @Column()
-  name: string;
-
-  @Column()
   email: string;
 
   @Column()
